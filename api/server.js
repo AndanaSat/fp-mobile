1d2e65f0d83ebdc1826436e6b6921bd293f6c16d
aud1tya4dnan <auditya.adnan2k3@gmail.com>
2023-12-05 10:23:52 +0700

    Update MySQL

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const { auth } = require("./firebase.js");
 const { createUserWithEmailAndPassword, signInWithEmailAndPassword } = require("firebase/auth");
 const { async } = require("@firebase/util");
 const { error } = require("console");
+const mysql = require('mysql2');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -43,6 +44,87 @@ app.post("/api/login", async(req, res) => {
         res.send("error").json({errorCode: errorCode, errorMessage: errorMessage});
     });
 })
+
+const db = mysql.createPool({
+    host: '192.168.3.172',
+    user: 'admin@localhost',
+    password: 'code01',
+    database: 'mobile',
+  });
+  
+  // db.connect((err) => {
+  //   if (err) {
+  //     console.log(err);
+  //     process.exit(1);
+  //   }
+  
+  //   console.log('Database connected');
+  // });
+
+app.get('/api/test', (req, res) => {
+    const sql = `SELECT * FROM tes`;
+    const query = db.query(sql, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Internal server error');
+      }
+    //   res.send("Sukses");
+      res.status(200).send(result);
+    });
+});
+
+app.post('/api/test', (req, res) => {
+    const uid = req.body.uid;
+    const distance = req.body.distance;
+    const timer = req.body.timer;
+  
+    const sql = `INSERT INTO tes (uid, distance, timer) VALUES (?, ?, ?)`;
+    const query = db.query(sql, [uid, distance, timer]);
+  
+    query.on('error', (err) => {
+      console.log(err);
+      res.status(500).send('Internal server error');
+    });
+  
+    query.on('end', () => {
+      res.status(200).send('Data tracking berhasil disimpan');
+    });
+});
+
+app.get('/api/tracking', (req, res) => {
+    const sql = `SELECT * FROM tracking`;
+    const query = db.query(sql, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Internal server error');
+      }
+  
+      res.status(200).send(result);
+    });
+});
+
+app.post('/api/image', (req, res) => {
+    const uid = req.body.uid;
+    // const dataTracking = req.body.dataTracking;
+    const image = req.body.image;
+  
+    const base64Image = Buffer.from(image, 'base64');
+  
+    // const sql = `INSERT INTO tracking_data (uid, data_tracking, image) VALUES (?, ?, ?)`;
+    // const query = db.query(sql, [uid, dataTracking, base64Image]);
+  
+    const sql = `INSERT INTO tracking (uid, image) VALUES (?, ?)`;
+    const query = db.query(sql, [uid, base64Image]);
+
+    query.on('error', (err) => {
+      console.log(err);
+      res.status(500).send('Internal server error');
+    });
+  
+    query.on('end', () => {
+      res.status(200).send('Data tracking berhasil disimpan');
+    });
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
